cfcd51d15a04e8281b1d0313021aeaf352e2b1c9
Noheemot <holarkunbi@gmail.com>
2020-01-29 15:22:00 +0100

    solved solution

--- Day1/solution.py
+++ Day1/solution.py
@@ -1,1 +1,54 @@
 
+#1. python --version
+
+#2. addition(+)
+numb1 = int(input("Enter the first number to know the result:"))
+numb2 = int(input("Enter the second number to know the result:"))
+addition = numb1+numb2
+print( "The Division  of the entered value is:", addition)
+#subtraction(-)
+subtract = numb2-numb1
+print("The Subtractin of the entered value is:",)
+#Multiplication(*)
+# number1 = 23
+# number2 = 34
+multiply = numb1 * numb2 
+print("The Division  of the entered value is:",multiply)
+#Modulus(%)
+modulus = numb2%numb1
+print("The Modulus of the entered value is:" ,modulus)
+
+#Division(/)
+division = numb2/numb1
+print("The Division  of the entered value is:","{:.2f}".format(division))
+
+#Exponential(**)
+exponent = numb1**2
+print("Expontial:",exponent)
+
+#FLOOR DIVISION (//)
+floor_division = numb2//numb1
+print("The floor of the function is:",floor_division)
+
+
+
+#3. writing on the editor
+print("Your first name:",input("Your first name: "))
+print("Your last name:",input("Your last name: "))
+print("Your age :",input("Your age : "))
+print("Your Country :",input("Your Country: "))
+print("I am enjoying learning of python")
+
+
+
+
+#4. checking data type
+print(type(10))
+print(type(9.8))
+print(type(3.14))
+print(type(4 - 4j))
+print(type(['Azeez', 'Python']))
+print(type("Your first name"))
+print(type("Your last name"))
+print(type("Your Age"))
+print(type("Your country"))
